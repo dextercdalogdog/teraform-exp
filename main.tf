53abfa766cf4b58d6d361921f2dcbba2ffe05461
Dexter Dalogdog <dexter@hypestacksinc.com>
2021-09-30 13:21:41 +0800

    load balancer

--- main.tf
+++ main.tf
@@ -69,3 +69,43 @@ resource "aws_route_table_association" "az-c" {
   subnet_id = aws_subnet.subnet-c.id
   route_table_id = aws_route_table.rt.id
 }
+
+resource "aws_security_group" "sg_public" {
+  name        = "public"
+  description = "public inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress  {
+      description      = "TLS from VPC"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  egress {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  tags = {
+    Name = "pulibc"
+  }
+}
+
+#Application Load Balancer
+resource "aws_lb" "alb" {
+  name               = "alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg_public.id]
+  subnets            = [aws_subnet.subnet-a.id,aws_subnet.subnet-b.id,aws_subnet.subnet-c.id]
+
+  enable_deletion_protection = true
+
+  tags = {
+    Environment = "test"
+  }
+}
